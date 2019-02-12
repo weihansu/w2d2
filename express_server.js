93fe53c8d6c1bfa16588a782ef6025669c61fd93
weihansu <weihan.sw@gmail.com>
2019-02-12 22:06:42 +0000

    add put method into urls/new route

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -47,7 +50,15 @@ app.get("/urls/:shortURL", (req, res) => {
   });
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
