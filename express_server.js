c59603b4029cfaa93a38fb653e7990d548e01482
weihansu <weihan.sw@gmail.com>
2019-02-12 17:16:25 +0000

    add urls.json to app.get

--- express_server.js
+++ express_server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const urtDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
